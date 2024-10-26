4ef8c7e173b56bfd137536237984927becae58c7
lgvespa <34505481+lgvespa@users.noreply.github.com>
2024-10-27 02:39:01 +0900

    いいねとリポストのショートカットキー追加及び無効化を非表示に変更
    - いいねとリポストのショートカットキー追加
    - 無効化を非表示に変更
    - HJKLキーでも操作できるように変更

--- tokimeki_mediakey_navigator.user.js
+++ tokimeki_mediakey_navigator.user.js
@@ -1,17 +1,38 @@
 // ==UserScript==
 // @name         Tokimeki Media Key Navaigator
 // @namespace    http://tampermonkey.net/
-// @version      0.0.2
+// @version      0.0.3
 // @description  TOKIMEKIの画像閲覧ページに矢印キーによる遷移機能を追加(画像表示一覧の表示時のみ)
-// @author       @heavy8.bsky.social
+// @author       lgvspa
 // @match        https://tokimeki.blue/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tokimeki.blue
 // @grant        none
 // ==/UserScript==
 
 (function() {
+    // ボタンの無効化表示が見にくいので灰色にするぐらいなら非表示にする
+    const css = `
+        button.splide__arrow[disabled]{
+            visibility: hidden;
+        }
+        `;
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+
+
     // 現在のメディアアイテムのインデックスを追跡
     let currentIndex = null;
+
+    function clickSelector(contentWrap, selector){
+        const element = contentWrap.querySelector(selector);
+        if (element){
+            element.click();
+        }else{
+            console.log(`${selector}が見つからない`);
+        }
+    }
 
     document.addEventListener('click', function(event) {
         const currentImg = event.target;
@@ -29,6 +50,7 @@
 
     document.addEventListener('keydown', function(event) {
         console.log(event);
+        // メディアビューアダイアログ(仮称)
         const contentWrap = document.querySelector('.media-content-wrap');
         if (contentWrap === null) return;
         if (currentIndex === null) return;
@@ -36,33 +58,42 @@
         const mediaItems = Array.from(document.querySelectorAll('.media-item'));
         const closeButton = contentWrap ? contentWrap.querySelector('.media-content-close') : null;
 
-        if (event.key === 'ArrowUp' && currentIndex > 0) {
+
+        // メディアビューアダイアログ表示中のみ有効
+        if (event.key === 'ArrowUp' || event.key === 'k' && currentIndex > 0) {
             // 上矢印キーが押された場合、前のメディアアイテムに移動
             event.preventDefault();
             currentIndex--;
-        } else if (event.key === 'ArrowDown' && currentIndex < mediaItems.length - 1) {
+        } else if (event.key === 'ArrowDown' || event.key === 'j'&& currentIndex < mediaItems.length - 1) {
             // 下矢印キーが押された場合、次のメディアアイテムに移動
             event.preventDefault();
             currentIndex++;
-        } else if (event.key === 'ArrowLeft') {
+        } else if (event.key === 'ArrowLeft' || event.key === 'h') {
             event.preventDefault();
-            const prevArrow = contentWrap.querySelector('.splide__arrow--prev');
-            if (prevArrow) prevArrow.click();
+            clickSelector(contentWrap, '.splide__arrow--prev');
             return;
-        } else if (event.key === 'ArrowRight') {
+        } else if (event.key === 'ArrowRight' || event.key === 'l') {
             event.preventDefault();
-            const nextArrow = contentWrap.querySelector('.splide__arrow--next');
-            if (nextArrow.click()) nextArrow.click();
+            clickSelector(contentWrap, '.splide__arrow--next');
+            return;
+        } else if (event.key === 'r') {
+            event.preventDefault();
+            clickSelector(contentWrap, '.timeline-reaction__item--repost');
+            currentIndex++;
+            return;
+        } else if (event.key === 'f' || event.key === ' ') {
+            event.preventDefault();
+            clickSelector(contentWrap, '.timeline-reaction__item--like');
+            currentIndex++;
             return;
         } else {
             return;
         }
 
         // console.log(`currentIndex: ${currentIndex}`);
+        // ダイアログを閉じたあと次の画像を表示
         if (closeButton) {
-            // .media-content-closeをクリックして閉じる
             closeButton.click();
-            // 遷移先の.media-itemをクリックして開く
             mediaItems[currentIndex].querySelector('img').click();
         }
     });
